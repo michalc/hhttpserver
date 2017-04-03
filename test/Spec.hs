e09a6b2a2e8bea47e1776f6a15995697a2ce5ad4
Michal Charemza <michal@charemza.name>
2017-04-03 20:32:01 +0100

    Shorter service names

--- test/Spec.hs
+++ test/Spec.hs
@@ -14,11 +14,11 @@ main = hspec $
 
     sessionWith config "integration" $ using allBrowsers $ do
       it "checks all text in p" $ runWD $ do
-        openPage "http://hhttpserver_run_for_test/test/files/index.html"
+        openPage "http://web_test/test/files/index.html"
         e <- findElem $ ByCSS "p"
         e `shouldHaveText` ("Some HTML")
 
       it "checks all text in p strong" $ runWD $ do
-        openPage "http://hhttpserver_run_for_test/test/files/index.html"
+        openPage "http://web_test/test/files/index.html"
         e <- findElem $ ByCSS "p strong"
         e `shouldHaveText` ("HTML")
